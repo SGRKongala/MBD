c0838c1006c85e05fecb06f41f734b4077d446f4
SGRKongala <119892001+SGRKongala@users.noreply.github.com>
2023-01-25 20:51:09 +0100

    Update words_series.py

--- words_per_year/words_series.py
+++ words_per_year/words_series.py
@@ -1,15 +1,19 @@
 path="/user/s2765918/newsarticle/combined_data_slim.csv"
 df =spark.read.options(header=True, inferSchema=True).option("multiline",'true').csv(path)
-df1 = df.select(lower(col("title")), year("date").alias("year"))
+#Change Date format to year and month
+df1 = df.select(lower(col("title")), date_format('date', 'yyyy-MM').alias("year"))
+#Processing the text and splitting words and to explode the column
 text_df = df1.withColumn("processed_text", regexp_replace("title", "[^a-zA-Z_\-]+", " "))
 text_df=text_df.select(col("year"), lower(col("processed_text")))
 df2=text_df.select(split(col("processed_text"), "\s+").alias("words"), col("year"))
 df3=df2.select(df2.year, explode(lower(df2.words)).alias("words"))
 df4=df4.select(df4.year, lower(df4.words).alias("word"))
+#Pivot the date column and count the words per month
 df5=df4.groupBy(df4.word).pivot("year").agg(count("word"))
 df6=df5.fillna(0)
 df7=df6.withColumn("total", sum(df6[col] for col in df6.columns[1:]))
 df7=df7.sort(df7.total.desc())
+#Remove stop words
 from pyspark.ml.feature import StopWordsRemover
 stopwordList = ["word1","word2","word3"]
 stopwordList.extend(StopWordsRemover().getStopWords())
